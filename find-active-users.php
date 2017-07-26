c0d2e68dc3f1edd310502eb07bd9de8079567704
Joshua Walcher <joshuawalcher@yahoo.com>
2017-07-26 13:30:46 -0500

    code style update

--- find-active-users.php
+++ find-active-users.php
@@ -11,24 +11,25 @@ $data = '';
 
 foreach ($sessions as $session) {
 	//init the variables on every loop
-  $first = '';
+  	$first = '';
 	$last = '';
 	$userId = '';
 	$lastLogin = '';
 	$email = '';
-  //the meta_value is a serialized array within another array that has a unique sessionid.
-  //Anyone know a way to bypass that key without a foreach? Sigh...
+  	
+	//the meta_value is a serialized array within another array that has a unique sessionid.
+ 	 //Anyone know a way to bypass that key without a foreach? Sigh...
 	$code = unserialize($session->meta_value);
 	foreach($code as $array){
 		//got the login timestamp
-    $lastLogin = $array['login'];
+    		$lastLogin = $array['login'];
 	}
 	$userId = (int)$session->user_id;
 	
-  //really, this may be all you need - the user id. But it might be useful to get the names and emails too.
+  	//really, this may be all you need - the user id. But it might be useful to get the names and emails too.
   
-  //get the first and last name
-  $pull = $wpdb->get_results("SELECT meta_key, meta_value FROM " . $wpdb->prefix . "usermeta WHERE meta_key IN ('first_name','last_name') AND user_id = '$userId'");
+  	//get the first and last name
+  	$pull = $wpdb->get_results("SELECT meta_key, meta_value FROM " . $wpdb->prefix . "usermeta WHERE meta_key IN ('first_name','last_name') AND user_id = '$userId'");
 	foreach($pull as $row){
 		if($row->meta_key == 'first_name'){
 			$first = $row->meta_value;
@@ -37,7 +38,7 @@ foreach ($sessions as $session) {
 		}
 	}
   
-  //get the email.
+  	//get the email.
 	$pull = $wpdb->get_results("SELECT user_email FROM " . $wpdb->prefix . "users where ID = '$userId'");
 	foreach($pull as $row){
 		$email = $row->user_email;
@@ -47,11 +48,13 @@ foreach ($sessions as $session) {
 	$data .= "$userId,$first,$last,$email," . date('m/d/Y',$lastLogin) . "\n";
 	
 }
+
 //create the file
 $fname = 'wordpress-users-and-last-login.csv';
 $fp = fopen($fname,'w');
 fwrite($fp,$data);
 fclose($fp);
+
 //attempt to stream to browser. Is failing for me in Chrome.
 header('Content-type: application/csv');
 header("Content-Disposition: inline; filename=".$fname);
